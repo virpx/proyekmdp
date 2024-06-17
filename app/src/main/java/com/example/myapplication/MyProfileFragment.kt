c091b1a66cacba4d9d3efc9a2141445ab97eee81
Muhammad robah <86682953+Daehoo0@users.noreply.github.com>
2024-06-18 00:35:31 +0700

    update nav
    nav log out tok

--- app/src/main/java/com/example/myapplication/MyProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/MyProfileFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.FragmentContainerView
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MyProfileFragment : Fragment() {
     lateinit var container: FragmentContainerView
+    private lateinit var logImage: ImageButton
+    private lateinit var logouttext: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +27,16 @@ class MyProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        logImage = view.findViewById(R.id.imageButton3)
+        logouttext = view.findViewById(R.id.textView28)
+
+        logouttext.setOnClickListener(){
+            container.getFragment<Fragment>().findNavController().navigate(R.id.action_global_loginFragment)
+        }
+        logImage.setOnClickListener(){
+            container.getFragment<Fragment>().findNavController().navigate(R.id.action_global_loginFragment)
+        }
 
         val navbuttom = view.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         navbuttom.setOnNavigationItemSelectedListener { menuItem ->
