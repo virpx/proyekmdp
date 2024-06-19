ebf8c40cf8846e7846ea5e70f62445c425130f03
ZeroEX04 <yoanesleonardi23@gmail.com>
2024-06-19 19:30:48 +0700

    host db

--- WEB_SERVICE/db.js
+++ WEB_SERVICE/db.js
@@ -38,10 +38,18 @@
 //   User,
 // };
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("proyek_mdp", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+require("dotenv").config(); // Load environment variables from .env file
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  }
+);
 
 const DChat = sequelize.define(
   "DChat",
